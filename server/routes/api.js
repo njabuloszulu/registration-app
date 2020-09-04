d38f2e05b64a89c0f2357b4b85af239f446c6e2e
njabuloszulu <njabuloszulu@gmail.com>
2020-09-04 10:18:30 +0200

    creating post request for new registration and get request to get the  registration list

---
+++ server/routes/api.js
@@ -0,0 +1,30 @@
+const Register = require("../models/register");
+
+const registerApi = (app) => {
+  app.post("/register", async (req, res) => {
+    try {
+      const { name, surname, email, idNumber, homeAddress } = req.body;
+      const newRegistration = await Register({
+        name,
+        surname,
+        email,
+        idNumber,
+        homeAddress,
+      }).save();
+      res.status(201).json(newRegistration);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  app.get("/register", async (req, res) => {
+    try {
+      const registrationList = await Register.find();
+      res.status(201).json(registrationList);
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  });
+};
+
+module.exports = { registerApi };
